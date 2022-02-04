a1cbfcddfadf030294a6bf703c1273f71214aeb9
jtamyrc <jason.tam@yource.cc>
2022-02-04 11:21:20 +0000

    add exclamation mark

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    A Pull Request demo here
+                    A Pull Request demo here!
                 </a>
             </header>
         </div>
